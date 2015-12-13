a18dddf69f26158720cfbc3f2d5a639c889e467f
zhouyou <mickey_zhouyou@yeah.net>
2015-12-13 21:44:08 +0800

    commitall

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
-#[test]
-fn it_works() {
-	assert!(true);
+
+pub enum SNode {
+	Leaf(i8, String),
+	Node(Box<Vec<SNode>>)
+}
+
+pub fn parse(exp:&str) -> SNode {
+	let vc = exp.chars().collect();
+	parse_chars(vc, 0, 0)
+}
+
+fn parse_chars(vc:Vec<char>, idx:usize, mut deep:usize) -> SNode {
+	let c = vc[idx];
+	if c == '(' {
+		deep += 1;
+	}
+	SNode::Leaf(0, "".to_string())
 }
---
+++ tests/lib.rs
@@ -0,0 +1,6 @@
+extern crate rust_s_exp;
+
+#[test]
+fn it_works() {
+	assert!(true);
+}
