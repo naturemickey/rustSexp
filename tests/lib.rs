16f478679dda588bee4653489ba5640ace35dd94
zhouyou <mickey_zhouyou@yeah.net>
2015-12-14 20:11:42 +0800

    commit

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,9 +1,10 @@
 extern crate rust_s_exp;
 
 use rust_s_exp::SNode::{Node, Leaf};
+use rust_s_exp::parse;
 
 #[test]
 fn it_works() {
 	let s = "(abc)";
-	assert!(true);
+	assert_eq!(parse(s), Node(vec![Leaf(0, "abc".to_string())]));
 }
