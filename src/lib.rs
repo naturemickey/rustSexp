035eaa42fcb86bd317b87bf4fca833bd5d058945
Michael Chow <mickey_zhouyou@yeah.net>
2015-12-14 18:01:47 +0800

    commitall

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,113 @@
 
+use std::option::Option::{None, Some};
+
 pub enum SNode {
 	// 0.标识， 1.字符串（不含双引号），后续有需要再加字段叶子类型
 	Leaf(i8, String),
 	Node(Box<Vec<SNode>>)
 }
 
-pub fn parse(exp:&str) -> SNode {
+pub fn parse(exp:&str) -> Vec<SNode> {
 	let vc = exp.chars().collect();
-	parse_chars(vc, 0, 0)
+	let (_, _, res) = read_snodes(&vc, 0, 0);
+	res
 }
 
-fn read_snodes(vc:Vec<char>, mut idx:usize, mut deep:usize) -> Vec<SNode> {
-	idx = pass_blank(&vc, idx);
+fn read_snodes(vc:&Vec<char>, idx:usize, deep:usize) -> (usize, usize, Vec<SNode>) {
 	if idx == vc.len() {
-		Vec::new()
+		(idx, deep, Vec::new())
 	} else {
-		let snodes = Vec::new();
-		let c = vc[idx];
-		if c != '(' {
-			panic!("S expression must be start with '('");
+		let mut snodes = Vec::new();
+		let mut i = idx;
+		let mut d = deep;
+		while let Some((new_idx, new_deep, snode)) = read_snode(vc, i, d) {
+			i = new_idx;
+			d = new_deep;
+			snodes.push(snode);
 		}
-		snodes
+		(i, d, snodes)
 	}
 }
 
-fn read_snode(Vec:Vec<char>, mut idx:usize) -> SNode {
-	SNode::Leaf(0, "".to_string())
+///
+/// return (new_idx, new_deep, SNode)
+///
+fn read_snode(vc:&Vec<char>, idx:usize, deep:usize) -> Option<(usize, usize, SNode)> {
+	let idx = pass_blank(vc, idx);
+	if idx == vc.len() {
+		None
+	} else {
+		let c = vc[idx];
+		if c == '(' {
+			
+		} else {
+			let (i, n) = read_leaf(vc, idx);
+			Some((i, deep, n))
+		}
+	}
 }
 
-fn parse_chars(vc:Vec<char>, mut idx:usize, mut deep:usize) -> SNode {
-	idx = pass_blank(&vc, idx);
+fn read_leaf(vc:&Vec<char>, idx:usize) -> (usize, SNode) {
 	let c = vc[idx];
-	if c != '(' {
-		panic!("S expression must be start with '('");
+
+	if c == '"' {
+		let mut s:String = String::new();
+		let mut idx_to:usize = idx + 1;
+		while (idx_to < vc.len()) {
+			if vc[idx_to] == '\\' {
+				if idx_to + 1 < vc.len() {
+					idx_to += 1;
+					s.push(vc[idx_to]);
+				} else {
+					panic!("unexpect EOF");
+				}
+			} else if vc[idx_to] != '"' {
+				s.push(vc[idx_to]);
+			} else {
+				break;
+			}
+			idx_to += 1;
+		}
+		if idx_to == vc.len() {
+			panic!("expect '\"' but find EOF");
+		}
+		(idx_to + 1, SNode::Leaf(1, s))
+	} else {
+		let mut s:String = String::new();
+		let mut idx_to:usize = idx + 1;
+		while (idx_to < vc.len()) {
+			if vc[idx_to] == '\\' {
+				if idx_to + 1 < vc.len() {
+					idx_to += 1;
+					s.push(vc[idx_to]);
+				} else {
+					panic!("unexpect EOF");
+				}
+			} else if is_not_blank(vc[idx_to]) && vc[idx_to] != ')' {
+				s.push(vc[idx_to]);
+			} else {
+				break;
+			}
+			idx_to += 1
+		}
+		(idx_to, SNode::Leaf(1, s))
 	}
-	deep += 1;
-	SNode::Leaf(0, "".to_string())
 }
 
 fn pass_blank(vc:&Vec<char>, idx:usize) -> usize {
 	for i in idx .. vc.len() {
 		let c = vc[i];
-		if !(c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+		if is_not_blank(c) {
 			return i;
 		}
 	}
 	vc.len()
+}
+
+fn is_blank(c:char) -> bool {
+	(c == ' ' || c == '\t' || c == '\n' || c == '\r')
+}
+
+fn is_not_blank(c:char) -> bool {
+	!is_blank(c)
 }
