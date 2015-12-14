5816ce42534472ab9b527d2746ded7aa05aaa785
Michael Chow <mickey_zhouyou@yeah.net>
2015-12-14 13:20:47 +0800

    commitall

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 
 pub enum SNode {
+	// 0.标识， 1.字符串（不含双引号），后续有需要再加字段叶子类型
 	Leaf(i8, String),
 	Node(Box<Vec<SNode>>)
 }
@@ -9,10 +10,40 @@ pub fn parse(exp:&str) -> SNode {
 	parse_chars(vc, 0, 0)
 }
 
-fn parse_chars(vc:Vec<char>, idx:usize, mut deep:usize) -> SNode {
-	let c = vc[idx];
-	if c == '(' {
-		deep += 1;
+fn read_snodes(vc:Vec<char>, mut idx:usize, mut deep:usize) -> Vec<SNode> {
+	idx = pass_blank(&vc, idx);
+	if idx == vc.len() {
+		Vec::new()
+	} else {
+		let snodes = Vec::new();
+		let c = vc[idx];
+		if c != '(' {
+			panic!("S expression must be start with '('");
+		}
+		snodes
 	}
+}
+
+fn read_snode(Vec:Vec<char>, mut idx:usize) -> SNode {
 	SNode::Leaf(0, "".to_string())
+}
+
+fn parse_chars(vc:Vec<char>, mut idx:usize, mut deep:usize) -> SNode {
+	idx = pass_blank(&vc, idx);
+	let c = vc[idx];
+	if c != '(' {
+		panic!("S expression must be start with '('");
+	}
+	deep += 1;
+	SNode::Leaf(0, "".to_string())
+}
+
+fn pass_blank(vc:&Vec<char>, idx:usize) -> usize {
+	for i in idx .. vc.len() {
+		let c = vc[i];
+		if !(c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+			return i;
+		}
+	}
+	vc.len()
 }
