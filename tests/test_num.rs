d0d4e05011e7ade411f9d37835d96e20e9a29336
Michael Chow <mickey_zhouyou@yeah.net>
2015-12-17 14:54:54 +0800

    commitall

--- tests/test_num.rs
+++ tests/test_num.rs
@@ -4,6 +4,7 @@ use rust_s_exp::is_number;
 
 #[test]
 fn it_works_num() {
+	// 合法数字
 	assert!(is_number("123"));
 	assert!(is_number("+12"));
 	assert!(is_number("-89"));
@@ -16,10 +17,13 @@ fn it_works_num() {
 	assert!(is_number("6."));
 	assert!(is_number("-345.876"));
 
+	// 非法数字
 	assert!(!is_number("+."));
 	assert!(!is_number("."));
 	assert!(!is_number("-"));
 	assert!(!is_number("0a9"));
 	assert!(!is_number("abcde"));
 	assert!(!is_number("+abc"));
+	assert!(!is_number("-+"));
+	assert!(!is_number("-0-1-"));
 }
